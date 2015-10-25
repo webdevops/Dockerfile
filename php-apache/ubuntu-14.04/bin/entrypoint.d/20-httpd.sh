9449b4261258983eec7498c99f418786a9078932
Markus Blaschke <mblaschke82@gmail.com>
2015-10-25 14:56:48 +0100

    Switched to ansible modular provisioning

--- php-apache/ubuntu-14.04/bin/entrypoint.d/20-httpd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HTTPD_CONF="/opt/docker/etc/httpd/vhost.conf"
-
-replaceTextInFile "<DOCUMENT_INDEX>" "${HTTPD_DOCUMENT_INDEX}" "$HTTPD_CONF"
-replaceTextInFile "<DOCUMENT_ROOT>"  "${HTTPD_DOCUMENT_ROOT}"  "$HTTPD_CONF"
-replaceTextInFile "<ALIAS_DOMAIN>"   "${HTTPD_ALIAS_DOMAIN}"   "$HTTPD_CONF"
-
-#echo "ServerName $(hostname)" >> /etc/httpd/conf.d/servername.conf
