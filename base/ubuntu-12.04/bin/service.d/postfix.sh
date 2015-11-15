91ca8a8d0fc927a4849d46bb2cc6ef04120c7b4f
Markus Blaschke <mblaschke82@gmail.com>
2015-11-15 19:46:37 +0100

    Fixed postfix wrapper

--- base/ubuntu-12.04/bin/service.d/postfix.sh
+++ base/ubuntu-12.04/bin/service.d/postfix.sh
@@ -23,7 +23,10 @@ trap "postfix reload" SIGHUP
 
 # force new copy of hosts there (otherwise links could be outdated)
 mkdir -p /var/spool/postfix/etc
-ln -sf /etc/hosts /var/spool/postfix/etc/hosts
+cp -f /etc/hosts       /var/spool/postfix/etc/hosts
+cp -f /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+cp -f /etc/services    /var/spool/postfix/etc/services
+
 
 # start postfix
 postfix start
