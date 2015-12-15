b136b44cc1216c009b43f833db4738c1c0fd7e9c
Markus Blaschke <mblaschke82@gmail.com>
2015-12-15 10:56:51 +0100

    Added deployment files for php7

--- .bin/provision.sh
+++ .bin/provision.sh
@@ -145,6 +145,9 @@ function deployProvision() {
     clearProvision  php  '*'
     deployProvision php/general       php  '*'
     deployProvision php/ubuntu-12.04  php  'ubuntu-12.04'
+
+    clearProvision  php  'debian-*-php7'
+    deployProvision php/debian-php7  php  'debian-*-php7'
 }
 
 [[ $(checkBuildTarget php-apache) ]] && {
@@ -153,6 +156,7 @@ function deployProvision() {
     deployProvision apache/general      php-apache  '*'
     deployProvision apache/centos       php-apache  'centos-*'
     deployProvision php-apache/general  php-apache  '*'
+    deployProvision php-apache/debian-php7  php-apache  'debian-*-php7'
 }
 
 [[ $(checkBuildTarget php-nginx) ]] && {
@@ -160,6 +164,7 @@ function deployProvision() {
     clearProvision  php-nginx  '*'
     deployProvision nginx/general      php-nginx  '*'
     deployProvision php-nginx/general  php-nginx  '*'
+    deployProvision php-nginx/debian-php7  php-nginx  'debian-*-php7'
 }
 
 exit 0
