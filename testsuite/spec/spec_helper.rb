7fd4e2bb3954bef9af4d9a43e26793556a3b9b8e
Markus Blaschke <mblaschke82@gmail.com>
2016-01-08 16:08:11 +0100

    Added wait_retry method

--- testsuite/spec/spec_helper.rb
+++ testsuite/spec/spec_helper.rb
@@ -8,3 +8,17 @@ Dir[base_spec_dir.join('shared/**/*.rb')].sort.each{ |f| require f }
 set :backend, :docker
 
 set :docker_container, ENV['TARGET']
+
+
+def wait_retry(time, increment = 5, elapsed_time = 0, &block)
+  begin
+    yield
+  rescue Exception => e
+    if elapsed_time >= time
+      raise e
+    else
+      sleep increment
+      wait_retry(time, increment, elapsed_time + increment, &block)
+    end
+  end
+end
