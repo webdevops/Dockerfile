a0c27ceb8785575368e39edfc9298a86de75a504
Markus Blaschke <mblaschke82@gmail.com>
2016-01-08 18:07:15 +0100

    Fixed hhvm tests

--- testsuite/spec/shared/hhvm/base.rb
+++ testsuite/spec/shared/hhvm/base.rb
@@ -2,7 +2,7 @@ shared_examples 'hhvm::listening::public' do
     describe port(9000) do
         it "hhvm should be listening" do
             wait_retry 15 do
-                should be_listening.on('0.0.0.0').with('tcp')
+                should be_listening.with('tcp6')
              end
         end
     end
@@ -12,7 +12,7 @@ shared_examples 'hhvm::listening::local-only' do
     describe port(9000) do
         it "hhvm should NOT be listening public" do
             wait_retry 15 do
-                should_not be_listening.on('0.0.0.0')
+                should_not be_listening
              end
         end
     end
@@ -20,7 +20,7 @@ shared_examples 'hhvm::listening::local-only' do
     describe port(9000) do
         it "hhvm should be listening local" do
             wait_retry 15 do
-                should be_listening.on('127.0.0.1').with('tcp')
+                should be_listening.on('::1').with('tcp6')
              end
         end
     end
