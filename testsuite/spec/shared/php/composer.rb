6af924a814f2c225d641c4f11d3df69bb6a0e88a
Markus Blaschke <mblaschke82@gmail.com>
2016-01-10 14:35:26 +0100

    Added composer file check

--- testsuite/spec/shared/php/composer.rb
+++ testsuite/spec/shared/php/composer.rb
@@ -1,5 +1,6 @@
 shared_examples 'php::composer' do
     it "should composer be installed" do
+        expect(file("/usr/local/bin/composer")).to be_file
         expect(file("/usr/local/bin/composer")).to be_executable
     end
 end
