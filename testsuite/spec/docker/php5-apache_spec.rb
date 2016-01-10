74705a1c34ae68706ed8f691728b6e02bd0b2947
Markus Blaschke <mblaschke82@gmail.com>
2016-01-10 13:55:42 +0100

    Fixed apache 2.2 configuration

--- testsuite/spec/docker/php5-apache_spec.rb
+++ testsuite/spec/docker/php5-apache_spec.rb
@@ -8,13 +8,19 @@ describe "Dockerfile" do
         set :docker_image, image.id
     end
 
+    include_examples 'php::cli'
+    include_examples 'php5::cli::version'
     include_examples 'php::modules'
     include_examples 'php5::modules'
+    include_examples 'php::cli::test::sha1'
     include_examples 'php::composer'
 
     # services
     include_examples 'php-fpm::listening::local-only'
     include_examples 'apache::listening::public'
+
+    # test after services are up
     include_examples 'apache::modules'
+    include_examples 'php::fpm::test::sha1'
 
 end
