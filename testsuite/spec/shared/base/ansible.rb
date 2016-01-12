8b7c982b2c693a1821679ca3d3d0e276bf1be7a5
Markus Blaschke <mblaschke82@gmail.com>
2016-01-12 22:02:17 +0100

    Added ansible version

--- testsuite/spec/shared/base/ansible.rb
+++ testsuite/spec/shared/base/ansible.rb
@@ -8,4 +8,10 @@ shared_examples 'base::ansible' do
             expect(file("/usr/local/bin/ansible-playbook")).to be_executable
         end
     end
+
+    describe command('ansible --version') do
+        its(:stdout) { should match %r!ansible 2.0.[0-9]+! }
+
+        its(:exit_status) { should eq 0 }
+    end
 end
