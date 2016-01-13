e63e2fb391e9b9f5b12c0ba05557bcdc9a1391f8
Markus Blaschke <mblaschke82@gmail.com>
2016-01-13 18:17:22 +0100

    Added graphicsmagick and imagemagick tests

--- testsuite/spec/collection/php5.rb
+++ testsuite/spec/collection/php5.rb
@@ -5,4 +5,7 @@ shared_examples 'collection::php5' do
     include_examples 'php5::modules'
     include_examples 'php::cli::test::sha1'
     include_examples 'php::composer'
+
+    include_examples 'misc::graphicsmagick'
+    include_examples 'misc::imagemagick'
 end
--- testsuite/spec/collection/php7.rb
+++ testsuite/spec/collection/php7.rb
@@ -5,4 +5,7 @@ shared_examples 'collection::php7' do
     include_examples 'php7::modules'
     include_examples 'php::cli::test::sha1'
     include_examples 'php::composer'
+
+    include_examples 'misc::graphicsmagick'
+    include_examples 'misc::imagemagick'
 end
