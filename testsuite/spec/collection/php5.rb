6f360efa51313657687077fe77d7b8385e180f32
Markus Blaschke <mblaschke82@gmail.com>
2016-01-15 20:31:58 +0100

    Added pear test

--- testsuite/spec/collection/php5.rb
+++ testsuite/spec/collection/php5.rb
@@ -5,6 +5,7 @@ shared_examples 'collection::php5' do
     include_examples 'php5::modules'
     include_examples 'php::cli::test::sha1'
     include_examples 'php::composer'
+    include_examples 'php::pear'
 
     include_examples 'misc::graphicsmagick'
     include_examples 'misc::imagemagick'
