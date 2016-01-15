fc7a29ec236470fb6d0f15d0f8bda269089cd9f5
Markus Blaschke <mblaschke82@gmail.com>
2016-01-15 20:31:52 +0100

    Fixed vsftp

--- testsuite/spec/shared/vsftp/user.rb
+++ testsuite/spec/shared/vsftp/user.rb
@@ -1,5 +1,5 @@
 shared_examples 'vsftp::user' do
-    describe user('dev') do
+    describe user('application') do
       it { should exist }
       it { should have_login_shell '/bin/bash' }
       its(:encrypted_password) { should match(/^\$6\$.{16}\$.{86}$/) }
