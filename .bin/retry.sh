d330733386ce7cf577da435e3b7081f9f12a1589
Markus Blaschke <mblaschke82@gmail.com>
2016-01-25 19:47:39 +0100

    Added retry.sh

---
+++ .bin/retry.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ -z "$RETRY_COUNT" ]; then
+    RETRY_COUNT=5
+fi
+
+if [ -z "$RETRY_DELAY" ]; then
+    RETRY_DELAY=1
+fi
+
+RETURN_CODE=0
+
+retry() {
+    local n=1
+
+    until [[ "$n" -ge "$RETRY_COUNT" ]]; do
+        RETURN_CODE="0"
+        "$@" && break || {
+            ((n++))
+            echo ""
+            echo " [WARNING] Command failed. Attempt $n/$RETRY_COUNT:"
+            echo ""
+            echo ""
+            RETURN_CODE=1
+            sleep 1;
+        }
+    done
+
+    if [[ "$RETURN_CODE" -ne 0 ]]; then
+        echo " [ERROR] The command has failed after $n attempts."
+    fi
+
+}
+
+retry $*
+
+exit "$RETURN_CODE"
