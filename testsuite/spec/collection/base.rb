452361bb10bc7fb74d084034f0fc8ac473d87249
Markus Blaschke <mblaschke82@gmail.com>
2016-01-28 00:31:33 +0100

    Removed base::localscripts

--- testsuite/spec/collection/base.rb
+++ testsuite/spec/collection/base.rb
@@ -4,7 +4,6 @@ shared_examples 'collection::base' do
     include_examples 'base::supervisor'
     include_examples 'base::syslog-ng'
     include_examples 'base::application-user'
-    include_examples 'base::localscripts'
 end
 
 shared_examples 'collection::base::without-application' do
@@ -12,5 +11,4 @@ shared_examples 'collection::base::without-application' do
     include_examples 'base::packages'
     include_examples 'base::supervisor'
     include_examples 'base::syslog-ng'
-    include_examples 'base::localscripts'
 end
--- testsuite/spec/shared/base/scripts.rb
+++
@@ -1,9 +0,0 @@
-shared_examples 'base::localscripts' do
-    it "should docker scripts be installed" do
-        expect(file("/usr/local/bin/apt-install")).to be_executable
-        expect(file("/usr/local/bin/apt-upgrade")).to be_executable
-        expect(file("/usr/local/bin/yum-install")).to be_executable
-        expect(file("/usr/local/bin/yum-upgrade")).to be_executable
-        expect(file("/usr/local/bin/service")).to be_executable
-    end
-end
