c6e500d1e8f8acbe28bc81f1e3a8924d55e2b9da
Markus Blaschke <mblaschke82@gmail.com>
2016-01-28 00:31:43 +0100

    Improved base::packages

--- testsuite/spec/shared/base/packages.rb
+++ testsuite/spec/shared/base/packages.rb
@@ -1,15 +1,20 @@
 shared_examples 'base::packages' do
-    describe package('logrotate')      do it { should be_installed } end
-    describe package('openssh-server') do it { should be_installed } end
-    describe package('dnsmasq')        do it { should be_installed } end
-    describe package('postfix')        do it { should be_installed } end
-    describe package('unzip')          do it { should be_installed } end
-    describe package('bzip2')          do it { should be_installed } end
-    describe package('wget')           do it { should be_installed } end
-    describe package('curl')           do it { should be_installed } end
-    describe package('moreutils')      do it { should be_installed } end
-    describe package('rsync')          do it { should be_installed } end
-    describe package('git')            do it { should be_installed } end
-    describe package('nano')           do it { should be_installed } end
-    describe package('net-tools')      do it { should be_installed } end
+    [
+        'openssh-server',
+        'dnsmasq',
+        'postfix',
+        'unzip',
+        'bzip2',
+        'wget',
+        'curl',
+        'moreutils',
+        'rsync',
+        'git',
+        'nano',
+        'net-tools',
+    ].each do |package|
+        describe package("#{package}") do
+            it { should be_installed }
+        end
+    end
 end
