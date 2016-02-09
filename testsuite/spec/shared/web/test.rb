089a68e45d0a1822b442e7d61e849e89915dab43
Markus Blaschke <mblaschke82@gmail.com>
2016-02-09 15:14:04 +0100

    Tried to fix curl issues

--- testsuite/spec/shared/web/test.rb
+++ testsuite/spec/shared/web/test.rb
@@ -3,7 +3,7 @@ shared_examples 'web::test::pi' do
         'http://localhost/pi-number.html',
         'https://localhost/pi-number.html'
     ].each do |url|
-        describe command("curl --insecure #{url}") do
+        describe command("curl --insecure --silent --retry 10 --fail #{url}") do
             its(:stdout) { should     contain('3.14159265359') }
 
             its(:exit_status) { should eq 0 }
