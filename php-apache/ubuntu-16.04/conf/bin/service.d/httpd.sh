1c1f2966598337656509f4a78b27f5890d5dc2fa
Markus Blaschke <mblaschke82@gmail.com>
2016-03-05 20:06:58 +0100

    Add warning if WEB_DOCUMENT_ROOT doesn't exists
    Fixed #48

--- php-apache/ubuntu-16.04/conf/bin/service.d/httpd.sh
+++ php-apache/ubuntu-16.04/conf/bin/service.d/httpd.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ ! -e "$WEB_DOCUMENT_ROOT" ]]; then
+    echo ""
+    echo "[WARNING] WEB_DOCUMENT_ROOT does not exists with path \"$WEB_DOCUMENT_ROOT\"!"
+    echo ""
+fi
+
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2*.pid
 
