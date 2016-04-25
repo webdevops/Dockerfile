ec79e3abdd0d227fba9b89a52bcf05973acee296
Markus Blaschke <mblaschke82@gmail.com>
2016-04-25 21:04:54 +0200

    Add db-cleanup for samson-deployment
    Related webdevops/samson-deployment#25

--- provisioning/samson-deployment/general/bin/service.d/samson.sh
+++ provisioning/samson-deployment/general/bin/service.d/samson.sh
@@ -9,9 +9,14 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 # Vacuum database
 #############################
 
-find /app/db/ -iname "*.sqlite3" | while read SQLITE_DATABSE; do
+find /storage/db/ -iname "*.sqlite3" | while read SQLITE_DATABSE; do
+    if [[ "$SQLITE_CLEANUP_DAYS" -gt 0 ]]; then
+        echo "Cleanup old deployment job logs (older than ${SQLITE_CLEANUP_DAYS} days)"
+        sqlite3 "$SQLITE_DATABSE" "UPDATE jobs SET output = '# Log cleared' WHERE created_at <= date('now', '-${SQLITE_CLEANUP_DAYS} day');"  || echo " -> ERROR: Cleanup failed"
+    fi
+
     echo "Running VACUUM on $SQLITE_DATABSE"
-    sqlite3 "$SQLITE_DATABSE" "VACUUM;"
+    sqlite3 "$SQLITE_DATABSE" "VACUUM;" || echo " -> ERROR: VACUUM failed"
 done
 
 sleep 1
