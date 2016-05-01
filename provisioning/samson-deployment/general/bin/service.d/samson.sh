21d29afb165e5ec35d7381f8ce2f62495a3c700e
Markus Blaschke <mblaschke82@gmail.com>
2016-05-01 14:58:38 +0200

    Fix samson-deployment startup errors

--- provisioning/samson-deployment/general/bin/service.d/samson.sh
+++ provisioning/samson-deployment/general/bin/service.d/samson.sh
@@ -9,14 +9,16 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 # Vacuum database
 #############################
 
-find /storage/db/ -iname "*.sqlite3" | while read SQLITE_DATABSE; do
-    if [[ "$SQLITE_CLEANUP_DAYS" -gt 0 ]]; then
-        echo "Cleanup old deployment job logs (older than ${SQLITE_CLEANUP_DAYS} days)"
-        sqlite3 "$SQLITE_DATABSE" "UPDATE jobs SET output = '# Log cleared' WHERE created_at <= date('now', '-${SQLITE_CLEANUP_DAYS} day');"  || echo " -> ERROR: Cleanup failed"
-    fi
+if [[ -d "/storage/db/" ]]; then
+    find /storage/db/ -iname "*.sqlite3" | while read SQLITE_DATABSE; do
+        if [[ "$SQLITE_CLEANUP_DAYS" -gt 0 ]]; then
+            echo "Cleanup old deployment job logs (older than ${SQLITE_CLEANUP_DAYS} days)"
+            sqlite3 "$SQLITE_DATABSE" "UPDATE jobs SET output = '# Log cleared' WHERE created_at <= date('now', '-${SQLITE_CLEANUP_DAYS} day');"  || echo " -> ERROR: Cleanup failed"
+        fi
 
-    echo "Running VACUUM on $SQLITE_DATABSE"
-    sqlite3 "$SQLITE_DATABSE" "VACUUM;" || echo " -> ERROR: VACUUM failed"
+        echo "Running VACUUM on $SQLITE_DATABSE"
+        sqlite3 "$SQLITE_DATABSE" "VACUUM;" || echo " -> ERROR: VACUUM failed"
+    done
 done
 
 sleep 1
