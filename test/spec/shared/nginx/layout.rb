82eb8cae86f27af4a1ce4b84f6bda3043fc35cc4
Markus Blaschke <mblaschke82@gmail.com>
2016-05-01 15:29:27 +0200

    Remove duplicate tests

--- test/spec/shared/nginx/layout.rb
+++ test/spec/shared/nginx/layout.rb
@@ -51,10 +51,6 @@ shared_examples 'nginx::layout' do
             it { should be_owned_by 'root' }
             it { should be_grouped_into 'root' }
 
-            # Owner test
-            it { should be_owned_by 'root' }
-            it { should be_grouped_into 'root' }
-
             # Read test
             it { should be_readable.by('owner') }
             it { should be_readable.by('group') }
