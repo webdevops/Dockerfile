169917354c9cce92281bd03219750d76c5429e38
Guillaume CAMUS <guillaume.camus@gmail.com>
2016-05-22 13:57:19 -0400

    :sparkles: Add container webdevops/php-apache-dev:centos-7-php56

---
+++ docker/php-apache-dev/centos-7-php56/conf/bin/service.d/httpd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ ! -e "$WEB_DOCUMENT_ROOT" ]]; then
+    echo ""
+    echo "[WARNING] WEB_DOCUMENT_ROOT does not exists with path \"$WEB_DOCUMENT_ROOT\"!"
+    echo ""
+fi
+
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/httpd/httpd.pid
+
+# Replace markers
+find /opt/docker/etc/httpd/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_INDEX>" "$WEB_DOCUMENT_INDEX"
+find /opt/docker/etc/httpd/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_ROOT>"  "$WEB_DOCUMENT_ROOT"
+find /opt/docker/etc/httpd/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<ALIAS_DOMAIN>"   "$WEB_ALIAS_DOMAIN"
+find /opt/docker/etc/httpd/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<SERVERNAME>"     "$HOSTNAME"
+
+exec httpd -DFOREGROUND
