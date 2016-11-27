1b15eda1617c9555bef7db8ff6bbbda80df9856e
Markus Blaschke <mblaschke82@gmail.com>
2016-11-27 21:13:51 +0100

    Add nginx-dev spec

---
+++ tests/serverspec/spec/docker/nginx-dev_spec.rb
@@ -0,0 +1,16 @@
+require 'serverspec'
+require 'docker'
+require 'spec_helper'
+
+describe "Dockerfile" do
+    before(:all) do
+        @image = Docker::Image.build_from_dir('.', { 'dockerfile' => ENV['DOCKERFILE'] })
+        set :docker_image, @image.id
+    end
+
+    include_examples 'collection::bootstrap'
+    include_examples 'collection::base'
+    include_examples 'collection::nginx'
+
+
+end
