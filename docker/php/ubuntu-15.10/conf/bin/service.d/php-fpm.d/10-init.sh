57080d27d4c111f9f1c9d6020bc33174d6903b6e
Markus Blaschke <mblaschke82@gmail.com>
2016-12-27 22:46:20 +0100

    Fix fpm/hhvm user setting when container runs for root

--- docker/php/ubuntu-15.10/conf/bin/service.d/php-fpm.d/10-init.sh
+++ docker/php/ubuntu-15.10/conf/bin/service.d/php-fpm.d/10-init.sh
@@ -2,5 +2,7 @@
 FPM_POOL_CONF="/opt/docker/etc/php/fpm/pool.d/application.conf"
 
 ## Setup container uid
-sed -i "s/user[ ]*=.*/user = ${CONTAINER_UID}/g"   "$FPM_POOL_CONF"
-sed -i "s/group[ ]*=.*/group = ${CONTAINER_UID}/g" "$FPM_POOL_CONF"
+if [[ "$CONTAINER_UID" != 0 ]]; then
+    sed -i "s/user[ ]*=.*/user = ${CONTAINER_UID}/g"   "$FPM_POOL_CONF"
+    sed -i "s/group[ ]*=.*/group = ${CONTAINER_UID}/g" "$FPM_POOL_CONF"
+fi
--- provisioning/hhvm/general/bin/service.d/hhvm.sh
+++ provisioning/hhvm/general/bin/service.d/hhvm.sh
@@ -4,4 +4,8 @@ source /opt/docker/bin/config.sh
 
 includeScriptDir "/opt/docker/bin/service.d/hhvm.d/"
 
+if [[ "$CONTAINER_UID" == 0 ]]; then
+    CONTAINER_UID="application"
+then
+
 exec /usr/bin/hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000 --user "${CONTAINER_UID}"
