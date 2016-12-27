ebb782f9549ff54c69dce8bea456ae4f2e254c5c
Markus Blaschke <mblaschke82@gmail.com>
2016-12-27 11:31:01 +0100

    Implement dynamic user setting of php-fpm

--- docker/php/debian-7/conf/bin/service.d/php-fpm.d/10-init.sh
+++ docker/php/debian-7/conf/bin/service.d/php-fpm.d/10-init.sh
@@ -1,1 +1,6 @@
-# placeholder
+# setup user env
+FPM_POOL_CONF="/opt/docker/etc/php/fpm/pool.d/application.conf"
+
+## Setup container uid
+sed -i "s/user[ ]*=.*/user = ${CONTAINER_UID}/g"   "$FPM_POOL_CONF"
+sed -i "s/group[ ]*=.*/group = ${CONTAINER_UID}/g" "$FPM_POOL_CONF"
