26e2e09be7c2c25605509ea2e65205c91b9c5aeb
Markus Blaschke <mblaschke82@gmail.com>
2016-12-27 11:48:10 +0100

    Implement dynamic user setting of hhvm

--- docker/hhvm/ubuntu-16.04/conf/bin/service.d/hhvm.sh
+++ docker/hhvm/ubuntu-16.04/conf/bin/service.d/hhvm.sh
@@ -4,4 +4,4 @@ source /opt/docker/bin/config.sh
 
 includeScriptDir "/opt/docker/bin/service.d/hhvm.d/"
 
-exec /usr/bin/hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000
+exec /usr/bin/hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000 --user "${CONTAINER_UID}"
