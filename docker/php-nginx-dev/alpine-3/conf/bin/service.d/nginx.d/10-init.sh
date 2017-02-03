fc1a9f8937de761981b4c8c375c0a02327dae957
Markus Blaschke <mblaschke82@gmail.com>
2017-02-03 12:41:12 +0100

    Mute replacements

--- docker/php-nginx-dev/alpine-3/conf/bin/service.d/nginx.d/10-init.sh
+++ docker/php-nginx-dev/alpine-3/conf/bin/service.d/nginx.d/10-init.sh
@@ -8,14 +8,14 @@ fi
 ln -f -s /var/lib/nginx/logs /var/log/nginx
 
 # Replace markers
-find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_INDEX>" "$WEB_DOCUMENT_INDEX"
-find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_ROOT>"  "$WEB_DOCUMENT_ROOT"
-find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<ALIAS_DOMAIN>"   "$WEB_ALIAS_DOMAIN"
-find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<SERVERNAME>"     "$HOSTNAME"
+find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_INDEX>" "$WEB_DOCUMENT_INDEX" > /dev/null
+find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<DOCUMENT_ROOT>"  "$WEB_DOCUMENT_ROOT" > /dev/null
+find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<ALIAS_DOMAIN>"   "$WEB_ALIAS_DOMAIN" > /dev/null
+find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<SERVERNAME>"     "$HOSTNAME" > /dev/null
 
 if [[ -n "${WEB_PHP_SOCKET+x}" ]]; then
     ## WEB_PHP_SOCKET is set
-    find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<PHP_SOCKET>" "$WEB_PHP_SOCKET"
+    find /opt/docker/etc/nginx/ -iname '*.conf' -print0 | xargs -0 -r rpl --quiet "<PHP_SOCKET>" "$WEB_PHP_SOCKET" > /dev/null
 else
     ## WEB_PHP_SOCKET is not set, remove PHP files
     rm -f -- /opt/docker/etc/nginx/conf.d/10-php.conf
