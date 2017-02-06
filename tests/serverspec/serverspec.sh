d3cd922c3d7b2d85a09f31f2b5a590f3759e4139
Markus Blaschke <mblaschke82@gmail.com>
2017-02-06 23:02:21 +0100

    Improve serverspec wrapper

--- tests/serverspec/serverspec.sh
+++ tests/serverspec/serverspec.sh
@@ -18,4 +18,34 @@ if [[ ! -f "${DOCKERFILE}" ]]; then
     fi
 fi
 
+# Check if DOCKER_IMAGE is set, needed for test
+if [[ -z "$DOCKER_IMAGE" ]]; then
+    echo "Environment variable 'DOCKER_IMAGE' not set"
+    exit 1
+fi
+
+# Check if DOCKER_TAG is set, needed for test
+if [[ -z "$DOCKER_TAG" ]]; then
+    echo "Environment variable 'DOCKER_TAG' not set"
+    exit 1
+fi
+
+# Check if OS_FAMILY is set, needed for test
+if [[ -z "$OS_FAMILY" ]]; then
+    echo "Environment variable 'OS_FAMILY' not set"
+    exit 1
+fi
+
+# Check if OS_FAMILY is set, needed for test
+if [[ -z "$OS_VERSION" ]]; then
+    echo "Environment variable 'OS_FAMILY' not set"
+    exit 1
+fi
+
+echo "Starting serverspec"
+echo "            OS: ${OS_FAMILY} Version ${OS_VERSION}"
+echo "  Docker image: ${DOCKER_IMAGE}:${DOCKER_TAG}"
+echo "    Dockerfile: ${DOCKERFILE}"
+echo ""
+
 exec bundle exec rspec "$@"
