ddb53bf6fb3b6c57fe5ea718dcdb3984d677a895
Markus Blaschke <mblaschke82@gmail.com>
2017-02-12 22:53:52 +0100

    Reduce retries for listening test to 20

--- tests/serverspec/spec/shared/typo3-solr/listening.rb
+++ tests/serverspec/spec/shared/typo3-solr/listening.rb
@@ -1,6 +1,6 @@
 shared_examples 'typo3-solr::listening::public' do
     describe port(8983) do
-        it "solr should be listening", :retry => 40, :retry_wait => 3 do
+        it "solr should be listening", :retry => 20, :retry_wait => 3 do
             should be_listening
         end
     end
