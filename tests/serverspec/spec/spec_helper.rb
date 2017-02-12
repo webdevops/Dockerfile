d950c6aa3eb917a8c10af214f4282df298ae3aca
Markus Blaschke <mblaschke82@gmail.com>
2017-02-12 15:32:00 +0100

    Add spec helper functions

--- tests/serverspec/spec/spec_helper.rb
+++ tests/serverspec/spec/spec_helper.rb
@@ -1,3 +1,38 @@
 #
-# placeholder for methods and stuff
+# Rspec methods
 #
+
+
+# Get pid of running service or process
+#
+# Example:
+#   service_get_pid("java")
+#     => 1
+def service_get_pid(process)
+    cmd = command("pidof " + process)
+    expect(cmd.exit_status).to eq 0
+
+    pid = cmd.stdout
+    pid.strip!
+
+    expect(pid).to match(%r!^[0-9]+$!)
+    expect(pid).not_to match(%r!^[0]+$!)
+
+    return pid
+end
+
+# Check if service/process is running stable for some time
+#
+# Example:
+#   service_running_check("java")
+def check_if_service_is_running_stable(process, wait_time = 10)
+    # check if service is up for the first time
+    service_pid = service_get_pid(process)
+
+    # wait some seconds to check if process was restarted
+    sleep(wait_time)
+
+    # recheck if service is still running
+    check_pid = service_get_pid(process)
+    expect(service_pid).to eq(check_pid)
+end
