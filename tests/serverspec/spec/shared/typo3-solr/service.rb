0b40f069200d31f8099873007f4bb29cb07e36ce
Markus Blaschke <mblaschke82@gmail.com>
2017-02-14 22:25:28 +0100

    Add and fix tests for typo3-solr:5.0

--- tests/serverspec/spec/shared/typo3-solr/service.rb
+++ tests/serverspec/spec/shared/typo3-solr/service.rb
@@ -1,6 +1,6 @@
 shared_examples 'typo3-solr::service::running' do
     describe "service solr check" do
-        it "should have running solr daemon", :retry => 20, :retry_wait => 3 do
+        it "should have running solr daemon", :retry => 10, :retry_wait => 3 do
             check_if_service_is_running_stable("java")
         end
     end
