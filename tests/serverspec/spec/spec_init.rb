6a9eb36b2897323b774c66759d142d15f195e7c3
Markus Blaschke <mblaschke82@gmail.com>
2017-04-10 23:05:42 +0200

    Print internal configuration hash in serverspec

--- tests/serverspec/spec/spec_init.rb
+++ tests/serverspec/spec/spec_init.rb
@@ -10,6 +10,12 @@ print "Serverspec configuration\n"
 print "------------------------\n"
 print " DOCKERIMAGE_ID: " + ENV['DOCKERIMAGE_ID'] + "\n"
 print " DOCKER_IMAGE: " + ENV['DOCKER_IMAGE'] + "\n"
+print " DOCKER_TAG: " + ENV['DOCKER_TAG'] + "\n"
 print " OS_FAMILY: " + ENV['OS_FAMILY'] + "\n"
 print " OS_VERSION: " + ENV['OS_VERSION'] + "\n"
 print "\n"
+print "--- internal config -----\n"
+$testConfiguration.each {|key, value| puts " #{key}: #{value}" }
+print "\n"
+
+
