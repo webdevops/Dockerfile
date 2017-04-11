ca18bf8359ba9a46412b3ee30f4a0ea1b58d630d
Markus Blaschke <mblaschke82@gmail.com>
2017-04-11 23:33:34 +0200

    Fix service provisioning

--- docker/base/debian-8/conf/provision/service.d/syslog.sh
+++ docker/base/debian-8/conf/provision/service.d/syslog.sh
@@ -19,15 +19,17 @@ esac
 SYSLOG_NG_VERSION=$(syslog-ng --version | grep -E -e '^syslog-ng[ ]+[0-9]+\.[0-9]+' | head -n 1 | awk '{print $2}' | cut -f 1,2 -d .)
 
 # Disable caps inside container
-go-replace --mode=lineinfile
-    -s "SYSLOGNG_OPTS" -r "SYSLOGNG_OPTS = --no-caps"
-    -- /etc/default/syslog-ng
+if [[ -f /etc/default/syslog-ng ]]; then
+    go-replace --mode=lineinfile \
+        -s "SYSLOGNG_OPTS" -r "SYSLOGNG_OPTS = --no-caps" \
+        -- /etc/default/syslog-ng
+fi
 
 # Symlink configuration
 ln -s -f /opt/docker/etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 
-go-replace --mode=lineinfile
-    -s "@version"      -r "@version: ${SYSLOG_NG_VERSION}"
+go-replace --mode=lineinfile \
+    -s "@version"      -r "@version: ${SYSLOG_NG_VERSION}" \
     -- /etc/syslog-ng/syslog-ng.conf
 
 # Ensure /var/lib/syslog-ng exists
