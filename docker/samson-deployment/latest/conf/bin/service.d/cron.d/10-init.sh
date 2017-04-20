4231c067b769d3ef64061c2284c437a1107a7ec6
Markus Blaschke <mblaschke82@gmail.com>
2017-04-20 18:29:41 +0200

    Add auto provision for crontab files

--- docker/samson-deployment/latest/conf/bin/service.d/cron.d/10-init.sh
+++ docker/samson-deployment/latest/conf/bin/service.d/cron.d/10-init.sh
@@ -1,1 +1,11 @@
-# placeholder
+# Install crontab files
+find /opt/docker/etc/cron -type f | while read CRONTAB_FILE; do
+    # fix permissions
+    chmod 0644 -- "$CRONTAB_FILE"
+
+    # add newline, cron needs this
+    echo >> "$CRONTAB_FILE"
+
+    # Install files
+    cp -a -- "$CRONTAB_FILE" "/etc/cron.d/$(basename "$CRONTAB_FILE")"
+done
