0f4adc0f4c24ec39f0a326dac38084d3d1e0badb
Markus Blaschke <mblaschke82@gmail.com>
2017-04-27 00:04:40 +0200

    Fix dnsmasq
    - Fix configuration file include
    - Fix running user (if container is not run privileged, dnsmasq only runs with root rights)

--- docker/php-official/7.1/conf/bin/service.d/dnsmasq.sh
+++ docker/php-official/7.1/conf/bin/service.d/dnsmasq.sh
@@ -2,6 +2,9 @@
 
 source /opt/docker/bin/config.sh
 
+DNSMASQ_USER="root"
+DNSMASQ_OPTS=""
+
 includeScriptDir "/opt/docker/bin/service.d/dnsmasq.d/"
 
-exec dnsmasq --keep-in-foreground
+exec dnsmasq --keep-in-foreground --user="$DNSMASQ_USER" $DNSMASQ_OPTS
