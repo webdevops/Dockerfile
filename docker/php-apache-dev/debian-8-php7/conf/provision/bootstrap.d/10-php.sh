aaf1e012122ad120d17bd275f3790ee00d3007d8
Markus Blaschke <mblaschke82@gmail.com>
2017-05-07 23:56:19 +0200

    Bugfixing

--- docker/php-apache-dev/debian-8-php7/conf/provision/bootstrap.d/10-php.sh
+++ docker/php-apache-dev/debian-8-php7/conf/provision/bootstrap.d/10-php.sh
@@ -2,6 +2,21 @@
 
 # Configure php-fpm
 go-replace --mode=lineinfile --regex \
-    -s '^[\s;]*listen[\s]*='         -r 'listen = 0.0.0.0:9000' \
     -s '^[\s;]*access.format[\s]*='  -r 'access.format = "%R - %u %t \"%m %r%Q%q\" %s %f cpu:%C%% mem:%{megabytes}M reqTime:%d"' \
     -- /opt/docker/etc/php/fpm/pool.d/application.conf
+
+if [[ "$(version-compare "$PHP_VERSION" "5.5.999")" == "<" ]]; then
+    # listen on public IPv4 port
+    # no ipv6 sockets available for old php version
+    go-replace --mode=lineinfile --regex \
+        -s '^[\s;]*listen[\s]*='                        -r 'listen = 0.0.0.0:9000' \
+        -- /opt/docker/etc/php/fpm/pool.d/application.conf \
+           /opt/docker/etc/php/fpm/php-fpm.conf
+else
+    # listen on public IPv6 port
+    go-replace --mode=lineinfile --regex \
+        -s '^[\s;]*listen[\s]*='                        -r 'listen = [::]:9000' \
+        -- /opt/docker/etc/php/fpm/pool.d/application.conf \
+           /opt/docker/etc/php/fpm/php-fpm.conf
+
+fi
--- docker/php-apache/7.0/conf/provision/bootstrap.d/10-php.sh
+++ docker/php-apache/7.0/conf/provision/bootstrap.d/10-php.sh
@@ -3,4 +3,5 @@
 # Restrict php-fpm to local connection
 go-replace --mode=lineinfile --regex \
     -s '^[\s;]*listen[\s]*='  -r 'listen = 127.0.0.1:9000' \
-    -- /opt/docker/etc/php/fpm/pool.d/application.conf
+    -- /opt/docker/etc/php/fpm/pool.d/application.conf \
+       /opt/docker/etc/php/fpm/php-fpm.conf
