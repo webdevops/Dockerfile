8308589270bb0637d4bf0f0faa01d9cfff43df5b
Markus Blaschke <mblaschke82@gmail.com>
2017-05-08 22:11:00 +0200

    Fix php setting

--- docker/php/alpine-php5/conf/provision/bootstrap.d/20-setup-php-fpm.sh
+++ docker/php/alpine-php5/conf/provision/bootstrap.d/20-setup-php-fpm.sh
@@ -9,6 +9,7 @@ ln -sf -- /opt/docker/etc/php/fpm/php-fpm.conf "$PHP_MAIN_CONF"
 
 # Configure php-fpm main (all versions)
 go-replace --mode=lineinfile --regex \
+    --lineinfile-after='\[global\]' \
     -s '^[\s;]*error_log[\s]*=' -r 'error_log = /docker.stderr' \
     -s '^[\s;]*pid[\s]*='       -r 'pid = /var/run/php-fpm.pid' \
     -- /opt/docker/etc/php/fpm/php-fpm.conf
@@ -17,12 +18,14 @@ if [[ "$(version-compare "$PHP_VERSION" "5.5.999")" == "<" ]]; then
     # listen on public IPv4 port
     # no ipv6 sockets available for old php version
     go-replace --mode=line --regex \
+        --lineinfile-after='\[global\]' \
         -s '^[\s;]*listen[\s]*=' -r 'listen = 0.0.0.0:9000' \
         -- /opt/docker/etc/php/fpm/pool.d/application.conf \
            /opt/docker/etc/php/fpm/php-fpm.conf
 else
     # listen on public IPv6 port
     go-replace --mode=line --regex \
+        --lineinfile-after='\[global\]' \
         -s '^[\s;]*listen[\s]*=' -r 'listen = [::]:9000' \
         -- /opt/docker/etc/php/fpm/pool.d/application.conf \
            /opt/docker/etc/php/fpm/php-fpm.conf
@@ -32,6 +35,7 @@ fi
 if [[ "$(version-compare "$PHP_VERSION" "5.99.999")" == "<" ]]; then
     # Configure php-fpm main (php 5.x)
     go-replace --mode=lineinfile --regex \
+        --lineinfile-after='\[global\]' \
         -s '^[\s;]*daemonize[\s]*=' -r 'daemonize = no' \
         -- /opt/docker/etc/php/fpm/php-fpm.conf
 fi
