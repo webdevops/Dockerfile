244233eb91172f43509c975610c7ba16311a1dbf
Markus Blaschke <mblaschke82@gmail.com>
2017-05-15 23:47:06 +0200

    Fix go-crond installation

--- docker/base/centos-7/conf/provision/service.d/cron.sh
+++ docker/base/centos-7/conf/provision/service.d/cron.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 GOCROND_VERSION=0.4.0
-wget -O /usr/local/bin/go-crond https://github.com/webdevops/go-crond/releases/download/$GOREPLACE_VERSION/go-crond-64-linux
+wget -O /usr/local/bin/go-crond https://github.com/webdevops/go-crond/releases/download/$GOCROND_VERSION/go-crond-64-linux
 chmod +x /usr/local/bin/go-crond
