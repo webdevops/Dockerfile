06fe513fcb0096f89f4aa1ad7ac4a7c58b37a2da
Markus Blaschke <mblaschke82@gmail.com>
2017-05-20 21:39:56 +0200

    Remove existing supervisor configuration in provision

--- docker/php-official/5.6/conf/provision/bootstrap.d/10-supervisor.sh
+++ docker/php-official/5.6/conf/provision/bootstrap.d/10-supervisor.sh
@@ -3,5 +3,8 @@
 # Remove /usr/sbin/service (images have custom service script)
 rm -rf /usr/sbin/service
 
+# Remove existing supervisor configuration
+rm -rf -- /etc/supervisor*
+
 # Link supervisor configuration script
 ln -sf /opt/docker/etc/supervisor.conf /etc/supervisord.conf
