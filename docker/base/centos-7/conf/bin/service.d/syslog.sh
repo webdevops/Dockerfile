7b398ec0f27b93dff2f4aa19aae6044d6324bfea
Markus Blaschke <mblaschke82@gmail.com>
2017-05-21 17:47:03 +0200

    Fix syslog

--- docker/base/centos-7/conf/bin/service.d/syslog.sh
+++ docker/base/centos-7/conf/bin/service.d/syslog.sh
@@ -5,6 +5,6 @@ if [[ -z "$SERVICE_SYSLOG_OPTS" ]]; then SERVICE_SYSLOG_OPTS=""; fi
 
 source /opt/docker/bin/config.sh
 
-includeScriptDir "/opt/docker/bin/service.d/syslog-ng.d/"
+includeScriptDir "/opt/docker/bin/service.d/syslog.d/"
 
-exec go-logd --syslog --pipe=stdout:/docker.stdout --pipe=stderr:/docker.stderr $SYSLOG_OPTS $SERVICE_SYSLOG_OPTS
+exec go-syslogd $SYSLOG_OPTS $SERVICE_SYSLOG_OPTS
