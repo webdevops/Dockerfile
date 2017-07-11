8c0ddada591fa062f643fb55cf120b6d8fc7918b
Markus Blaschke <mblaschke82@gmail.com>
2017-07-12 00:49:59 +0200

    Remove wrong echo

--- docker/base/alpine/conf/provision/service.d/postfix.sh
+++ docker/base/alpine/conf/provision/service.d/postfix.sh
@@ -13,7 +13,7 @@ case "$IMAGE_FAMILY" in
 
         # Fix mysql lib
         if [[ ! -f /lib64/libmysqlclient.so.18 ]] && [[ -f /usr/lib64/mysql/libmysqlclient.so.18 ]]; then
-            echo ln -s /usr/lib64/mysql/libmysqlclient.so.18 /lib64/libmysqlclient.so.18
+            ln -s /usr/lib64/mysql/libmysqlclient.so.18 /lib64/libmysqlclient.so.18
         fi
         ;;
 
