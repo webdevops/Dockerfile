b75cc152cab282abb7af07c2a4af8adf39e0007e
Hans Höchtl <hhoechtl@1drop.de>
2018-10-08 20:39:16 +0200

    [TASK] add ioncube to all php images
    Resolves #52

--- docker/php-official/5.6/conf/provision/bootstrap.d/11-php-conf.sh
+++ docker/php-official/5.6/conf/provision/bootstrap.d/11-php-conf.sh
@@ -7,3 +7,4 @@ PHP_MAIN_CONF=/usr/local/etc/php-fpm.conf
 PHP_POOL_CONF=www.conf
 PHP_POOL_DIR=/usr/local/etc/php-fpm.d
 PHP_FPM_BIN=/usr/local/sbin/php-fpm
+PHP_MOD_INI_DIR=/usr/local/etc/php/conf.d
