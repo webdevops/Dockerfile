20cb068d74905247d1aa88339a31fe5e67b091c7
Hans Höchtl <hhoechtl@1drop.de>
2020-04-13 18:01:02 +0200

    fix alpine cron and httpd

--- docker/php-apache/7.4-alpine/conf/bin/service.d/httpd.sh
+++ docker/php-apache/7.4-alpine/conf/bin/service.d/httpd.sh
@@ -8,7 +8,6 @@ source /opt/docker/bin/config.sh
 includeScriptDir "/opt/docker/bin/service.d/httpd.d/"
 
 # Apache gets grumpy about PID files pre-existing
-rm -f /var/run/apache2/apache2*.pid
+rm -f /var/run/httpd/httpd.pid
 
-source /etc/apache2/envvars
-exec apache2 -DFOREGROUND -DAPACHE_LOCK_DIR $SERVICE_APACHE_OPTS
+exec /usr/sbin/apachectl -DFOREGROUND $SERVICE_APACHE_OPTS
