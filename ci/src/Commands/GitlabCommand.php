3f70490c180410bbb524524d3109421144fbadd3
Hans Höchtl <hhoechtl@1drop.de>
2020-04-13 16:08:21 +0200

    add blacklist which leads to manual jobs

--- ci/src/Commands/GitlabCommand.php
+++ ci/src/Commands/GitlabCommand.php
@@ -7,6 +7,7 @@ use BlueM\Tree\Node;
 use BlueM\Tree\Serializer\HierarchicalTreeJsonSerializer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 use Webdevops\Build\FileReader;
@@ -19,6 +20,7 @@ class GitlabCommand extends Command
     protected $jobBuilder;
     protected $jobs = [];
     protected $deepestLevel = 0;
+    protected $blacklist = [];
 
     protected static $defaultName = 'gitlab:generate-ci';
 
@@ -27,10 +29,15 @@ class GitlabCommand extends Command
         $this->fileReader = new FileReader();
         $this->jobBuilder = new JobBuilder();
         parent::__construct();
+        $this->addOption('blacklist', 'b', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->blacklist = $input->getOption('blacklist');
+        if (empty($this->blacklist) && file_exists(__DIR__ . '/../../BLACKLIST')) {
+            $this->blacklist = file(__DIR__ . '/../../BLACKLIST', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        }
         $this->output = $output;
         $tree = $this->buildTree();
         foreach ($tree->getRootNodes() as $rootNode) {
@@ -39,21 +46,36 @@ class GitlabCommand extends Command
         $gitlabCi['stages'] = array_map(function($level) {return 'level' . $level;}, range(1, $this->deepestLevel));
         $yaml = Yaml::dump(array_merge($gitlabCi, $this->jobs), 3, 2);
         file_put_contents(__DIR__ . '/../../../.gitlab-ci.yml', $yaml);
+        return 0;
     }
 
     private function traverse(Node $node)
     {
         $line = 'Processing ' . $node->getName();
-        $this->output->write([str_pad('', $node->getLevel() - 1, "\t", STR_PAD_LEFT), $line, PHP_EOL]);
         $nodeAr = $node->toArray();
         $nodeAr['level'] = $node->getLevel();
         if ($node->getLevel() > $this->deepestLevel) {
             $this->deepestLevel = $node->getLevel();
         }
         $this->jobs[$node->getId()] = $this->jobBuilder->getJobDescription($nodeAr);
+        if ($this->isNameBlacklisted($nodeAr['id'])) {
+            $this->jobs[$node->getId()] = array_merge($this->jobs[$node->getId()], ['when' => 'manual']);
+            $line .= ' *blacklisted*';
+        }
+        $this->output->write([str_pad('', $node->getLevel() - 1, "\t", STR_PAD_LEFT), $line, PHP_EOL]);
         foreach ($node->getChildren() as $childNode) {
             $this->traverse($childNode);
         }
+    }
+
+    private function isNameBlacklisted(string $name)
+    {
+        foreach ($this->blacklist as $blacklistItem) {
+            if (strpos($name, $blacklistItem)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private function buildTree()
