015d1549e3c5368b73d02af0b95e6b20d25ceda6
Hans Höchtl <hhoechtl@1drop.de>
2020-04-13 16:25:51 +0200

    fix php ini provisioning in alpine

--- docker/php-official/7.3-alpine/conf/provision/bootstrap.d/20-setup-php.sh
+++ docker/php-official/7.3-alpine/conf/provision/bootstrap.d/20-setup-php.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 case "$IMAGE_FAMILY" in
-    Debian|Ubuntu)
+    Debian|Ubuntu|Alpine)
         # Register webdevops ini
         ln -sf "/opt/docker/etc/php/php.webdevops.ini" "${PHP_ETC_DIR}/conf.d/98-webdevops.ini"
 
