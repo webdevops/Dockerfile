8f981bb239945b9d73c19f482c5fa52089693494
Hans Höchtl <hhoechtl@1drop.de>
2020-10-27 12:45:22 +0100

    fix serverspec for composer

--- tests/serverspec/spec/shared/php/composer.rb
+++ tests/serverspec/spec/shared/php/composer.rb
@@ -1,7 +1,12 @@
 shared_examples 'php::composer' do
-    it "should composer be installed" do
-        expect(file("/usr/local/bin/composer")).to be_file
-        expect(file("/usr/local/bin/composer")).to be_executable
+    it "should composer V1 be installed" do
+        expect(file("/usr/local/bin/composer1")).to be_file
+        expect(file("/usr/local/bin/composer1")).to be_executable
+    end
+
+    it "should composer V2 be installed" do
+        expect(file("/usr/local/bin/composer2")).to be_file
+        expect(file("/usr/local/bin/composer2")).to be_executable
     end
 
     describe command('/usr/local/bin/composer') do
