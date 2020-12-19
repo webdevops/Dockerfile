1ad1cd74117dc5b504e5aeb7cf5c85019b1d698e
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2020-12-19 20:39:32 +0100

    [webdevops/Dockerfile#226] add teardown phase for supervisor task
    * restore gosu suid bit when container stops
    
    related to:
    * [webdevops/Dockerfile#226]
    * [webdevops/Dockerfile#366]

--- docker/base/ubuntu-17.04/conf/bin/entrypoint.d/cli.sh
+++ docker/base/ubuntu-17.04/conf/bin/entrypoint.d/cli.sh
@@ -5,13 +5,13 @@
 #############################################
 
 if [ -n "${CLI_SCRIPT}" ]; then
-    if [ -n "$APPLICATION_USER" ]; then
+    if [ -n "${CONTAINER_UID}" ]; then
         # Run as EFFECTIVE_USER
         shift
-        exec gosu "${APPLICATION_USER}" ${CLI_SCRIPT} "$@"
+        exec gosu "${CONTAINER_UID}" "${CLI_SCRIPT}" "$@"
     else
         # Run as root
-        exec ${CLI_SCRIPT} "$@"
+        exec "${CLI_SCRIPT}" "$@"
     fi
 else
     echo "[ERROR] No CLI_SCRIPT in in docker environment defined"
--- provisioning/base/general/bin/entrypoint.sh
+++ provisioning/base/general/bin/entrypoint.sh
@@ -4,43 +4,32 @@ if [[ -z "$CONTAINER_UID" ]]; then
     export CONTAINER_UID="application"
 fi
 
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o pipefail # trace ERR through pipes
+set -o errtrace # trace ERR through 'time command' and other functions
+set -o nounset  ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value
 
 # auto elevate privileges (if container is not started as root)
 if [[ "$UID" -ne 0 ]]; then
     export CONTAINER_UID="$UID"
     exec gosu root "$0" "$@"
 fi
-# remove suid bit on gosu
-chmod -s /sbin/gosu
 
-trap 'echo sigterm ; exit' SIGTERM
-trap 'echo sigkill ; exit' SIGKILL
-
-# sanitize input and set task
-TASK="$(echo $1| sed 's/[^-_a-zA-Z0-9]*//g')"
-
-source /opt/docker/bin/config.sh
-
+. /opt/docker/bin/config.sh
 createDockerStdoutStderr
 
-if [[ "$UID" -eq 0 ]]; then
-    # Only run provision if user is root
+# sanitize input and set task
+TASK="$(echo $1 | sed 's/[^-_a-zA-Z0-9]*//g')"
 
-    if [ "$TASK" == "supervisord" -o "$TASK" == "noop" ]; then
-        # Visible provisioning
-        runProvisionEntrypoint
-    else
-        # Hidden provisioning
-        runProvisionEntrypoint  > /dev/null
-    fi
+if [ "$TASK" == "supervisord" ] || [ "$TASK" == "noop" ]; then
+    # visible provisioning
+    runProvisionEntrypoint
+    trap 'runTeardownEntrypoint' SIGTERM
+    runEntrypoints "$@" &
+    wait $!
+    runTeardownEntrypoint
+else
+    # hidden provisioning
+    runProvisionEntrypoint > /dev/null
+    runEntrypoints "$@"
 fi
-
-#############################
-## COMMAND
-#############################
-
-runEntrypoints "$@"
