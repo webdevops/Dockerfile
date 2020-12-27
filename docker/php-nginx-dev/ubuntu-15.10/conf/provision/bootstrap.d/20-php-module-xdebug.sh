aac61bf3cb14529cbdf9a8710c805c8f0eab304d
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2020-12-27 16:30:19 +0100

    [webdevops/Dockerfile#384] make provision

--- docker/php-nginx-dev/ubuntu-15.10/conf/provision/bootstrap.d/20-php-module-xdebug.sh
+++ docker/php-nginx-dev/ubuntu-15.10/conf/provision/bootstrap.d/20-php-module-xdebug.sh
@@ -22,25 +22,29 @@ case "$IMAGE_FAMILY" in
 esac
 
 # Configure xdebug for development
-XDEBUG_VERSION=$(php -r "echo phpversion('xdebug');")
+declare -A XDEBUG
+XDEBUG[version]=$(php -r "echo phpversion('xdebug');")
+
+if [[ "$(version-compare "${XDEBUG[version]}" "3.0.0")" == "<" ]]; then
+    XDEBUG[remote_enable]='xdebug.remote_enable = 1'
+    XDEBUG[remote_connect_back]='xdebug.remote_connect_back = 1'
+    XDEBUG[profiler_output_dir]='xdebug.profiler_output_dir = /tmp/debug'
+else
+    XDEBUG[mode]='xdebug.mode = debug'
+    XDEBUG[discover_client_host]='xdebug.discover_client_host = 1'
+    XDEBUG[start_with_request]='xdebug.start_with_request = trigger'
+    XDEBUG[output_dir]='xdebug.output_dir = /tmp/debug'
+fi
 
 go-replace --mode=lineinfile --regex \
     -s '^[\s;]*xdebug.idekey[\s]*='                   -r 'xdebug.idekey = docker' \
     -s '^[\s;]*xdebug.cli_color[\s]*='                -r 'xdebug.cli_color = 1' \
     -s '^[\s;]*xdebug.max_nesting_level[\s]*='        -r 'xdebug.max_nesting_level = 1000' \
+    -s '^[\s;]*xdebug.remote_enable[\s]*='            -r "${XDEBUG[remote_enable]:-}" \
+    -s '^[\s;]*xdebug.remote_connect_back[\s]*='      -r "${XDEBUG[remote_connect_back]:-}" \
+    -s '^[\s;]*xdebug.profiler_output_dir[\s]*='      -r "${XDEBUG[profiler_output_dir]:-}" \
+    -s '^[\s;]*xdebug.mode[\s]*='                     -r "${XDEBUG[mode]:-}" \
+    -s '^[\s;]*xdebug.discover_client_host[\s]*='     -r "${XDEBUG[discover_client_host]:-}" \
+    -s '^[\s;]*xdebug.start_with_request[\s]*='       -r "${XDEBUG[start_with_request]:-}" \
+    -s '^[\s;]*xdebug.output_dir[\s]*='               -r "${XDEBUG[output_dir]:-}" \
     -- /opt/docker/etc/php/php.webdevops.ini
-
-if [[ "$(version-compare "$XDEBUG_VERSION" "3.0.0")" == "<" ]]; then
-    go-replace --mode=lineinfile --regex \
-        -s '^[\s;]*xdebug.remote_enable[\s]*='            -r 'xdebug.remote_enable = 1' \
-        -s '^[\s;]*xdebug.remote_connect_back[\s]*='      -r 'xdebug.remote_connect_back = 1' \
-        -s '^[\s;]*xdebug.profiler_output_dir[\s]*='      -r 'xdebug.profiler_output_dir = /tmp/debug' \
-        -- /opt/docker/etc/php/php.webdevops.ini
-else
-    go-replace --mode=lineinfile --regex \
-        -s '^[\s;]*xdebug.mode[\s]*='                     -r 'xdebug.mode = debug' \
-        -s '^[\s;]*xdebug.discover_client_host[\s]*='     -r 'xdebug.discover_client_host = 1' \
-        -s '^[\s;]*xdebug.start_with_request[\s]*='       -r 'xdebug.start_with_request = trigger' \
-        -s '^[\s;]*xdebug.output_dir[\s]*='               -r 'xdebug.output_dir = /tmp/debug' \
-        -- /opt/docker/etc/php/php.webdevops.ini
-fi
